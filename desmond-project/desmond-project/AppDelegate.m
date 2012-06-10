071afcadb40412a41019dd87d2652412735b18b5
Ignacio Valdivieso <igvdjn@gmail.com>
2012-06-10 16:27:06 -0300

    cancel all local notifications when entering foreground

--- desmond-project/desmond-project/AppDelegate.m
+++ desmond-project/desmond-project/AppDelegate.m
@@ -78,6 +78,8 @@
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
+    
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
